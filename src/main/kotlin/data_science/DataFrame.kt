99fcea7694cd89595bee068d452e4c335f74da79
Parimal Matte <123257338+PmCoder26@users.noreply.github.com>
2025-06-27 16:19:20 +0530

    data_science/DataFrame: covered dataframe operations such as column selection, dropping rows, removing comlumns, sorting and shuffling rows, reversing dataframe rows, grouping the dataframe commit

--- src/main/kotlin/data_science/DataFrame.kt
+++ src/main/kotlin/data_science/DataFrame.kt
@@ -7,6 +7,7 @@ import org.jetbrains.kotlinx.dataframe.api.columnOf
 import org.jetbrains.kotlinx.dataframe.api.dataFrameOf
 import org.jetbrains.kotlinx.dataframe.api.next
 import org.jetbrains.kotlinx.dataframe.api.prev
+import org.jetbrains.kotlinx.dataframe.get
 import org.jetbrains.kotlinx.dataframe.io.readCsv
 import org.jetbrains.kotlinx.dataframe.io.toHtml
 import java.io.File
@@ -79,18 +80,22 @@ fun main() {
 
 
             // Other basic Dataframe operations
-    var df = DataFrame.readCsv("src/main/kotlin/data_science/data.csv")
+//    var df = DataFrame.readCsv("src/main/kotlin/data_science/data.csv")
     // head operation (accessing first n rows)
 //    println(df.head(7))     // default: 5
+
     // tail operation (accessing last n rows)
 //    println(df.tail(7))     // default: 5
+
     // describe operation for summary statistics.
 //    println(df.describe())    // for all columns.
 //    println(df.describe("Age", "BMI", "Glucose"))     // for specific columns.
+
     // iterating over all rows of df.
 //    for(row in df) {
 //        println(row["Age"])
 //    }
+
     // filtering operation.
 //    val result = df.filter { "Age"<Int>() % 2 == 0 }
 //    println("DF with ages of odd numbers is as follows")
@@ -98,7 +103,65 @@ fun main() {
 //    val result = df.filter { ("Glucose"<Int>() > 80).and("Insulin"<Int>() > 2.0) }
 //    println("DF with glucose > 80 and Insulin > 2.0 is as follows")
 //    println(result)
-    //
+
+    // selecting the columns from the dataframe.
+//    println(df["Age", "Glucose"])
+
+//    var df = dataFrameOf("Name", "Age", "City", "Gender")(
+//        "Alice", 25, "New York", "F",
+//        "Bob", 30, "Delhi", "M",
+//        "Charlie", 28, "Paris", "M"
+//    )
+    // dropping the rows: Removes all rows that satisfy row condition (not inPlace operation)
+//    val dfDropped = df.drop { it["Name"] == "Bob" }
+//    println("Rows don't contain the name as Bob is")
+//    println(dfDropped)
+
+    // removing the columns:
+//    val dfRemoved = df.remove("Age", "Gender")
+//    println("Original df is:")
+//    println(df)
+//    println("Removed df is: ")
+//    println(dfRemoved)
+
+    // sorting the rows.
+//    val dfSorted = df.sortBy("Age")   // Returns DataFrame with rows sorted by one or several columns.
+//    val dfSorted = df.sortByDesc("Age")   // Returns DataFrame sorted by one or several columns in descending order.
+//    println("Sorted df is: ")
+//    println(dfSorted)
+
+    // shuffle the rows: Returns DataFrame with randomly reordered rows.
+//    val dfShuffled = df.shuffle()
+//    println("Shuffled df is: ")
+//    println(dfShuffled)
+
+    // reversing the rows' order: returns DataFrame with rows in reversed order.
+//    val dfReversed = df.reverse()
+//    println("Reversed df is: ")
+//    println(dfReversed)
+
+    val df = dataFrameOf("firstName", "lastName", "age", "city", "weight", "isHappy")(
+        "Alice", "Cooper", 15, "London", 54, true,
+        "Bob", "Dylan", 45, "Dubai", 87, true,
+        "Charlie", "Daniels", 20, "Moscow", null, false,
+        "Charlie", "Chaplin", 40, "Milan", null, true,
+        "Bob", "Marley", 30, "Tokyo", 68, true,
+        "Alice", "Wolf", 20, null, 55, false,
+        "Charlie", "Byrd", 30, "Moscow", 90, true
+    )
+    // grouping the dataframe.
+    println("Grouped by firstName is: ")
+    println()
+    val dfGrouped = df.groupBy("firstName")
+    dfGrouped.forEach { it ->
+        println("***********************************************************")
+        println("Group is: ${it.key["firstName"]}")
+        println(it.group)
+        println("***********************************************************")
+    }
+
+
+
 
 
 
